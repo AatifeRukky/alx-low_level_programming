e7bae40b279c4bd8efaa9bed7854ca994f4d4f77
AatifeRukky <iyanuoluwaaatife@gmail.com>
2022-07-04 17:18:19 -0700

    tyr 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+       int n;
+       int lastn;
+
+       srand(time(0));
+       n = rand() - RAND_MAX / 2;
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+*
+/
