0ba48a8206cb8996c4a287c8583cffc51f049447
AatifeRukky <iyanuoluwaaatife@gmail.com>
2022-07-07 16:26:37 -0700

    fix 2

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,7 @@
 /**
  * print_diagonal - a function that draws a diagonal line on the terminal.
  * @n: An input integer
- * Return: Always 0
+ * Return: always 0
  */
 void print_diagonal(int n)
 {
@@ -18,8 +18,7 @@ void print_diagonal(int n)
 		_putchar(92);
 		_putchar('n\');
 		}
-	
 	}
 	else
-		_putchar('n\');
+		_putchar('\n');
 }
