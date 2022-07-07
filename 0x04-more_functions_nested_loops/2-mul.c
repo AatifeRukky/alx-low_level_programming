9a7cc37a40c8929b0607e5fc9c64e717e19d5e57
AatifeRukky <iyanuoluwaaatife@gmail.com>
2022-07-07 14:47:45 -0700

    i think i am beginning to geta hang of this

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul - mlutiplies two ints.
+ *@a: integer.
+ *@b: integer.
+*
+ *Return: product.
+*/
+int mul(int a, int b)
+{
+	int product:
+
+	product = a * b;
+	return (product);
+}
