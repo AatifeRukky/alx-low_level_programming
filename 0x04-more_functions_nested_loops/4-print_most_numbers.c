ed3e6b52a76619485ced0772bae6025c61bd81c6
AatifeRukky <iyanuoluwaaatife@gmail.com>
2022-07-07 15:40:14 -0700

    try 2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9.
- *Do not print 2 and 4.
- *followed by another line.
+ * Do not print 2 and 4.
+ * followed by a new line.
  *Return: Always 0
  */
 void print_most_numbers(void)
 {
-	int 1 = '0';
+	int i = '0';
 
 	for (; i <= '9'; i++)
 	{
