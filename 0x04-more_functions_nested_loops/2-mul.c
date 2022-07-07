a8aa600924e1bcfe1e217abad82d34d795b2cdff
AatifeRukky <iyanuoluwaaatife@gmail.com>
2022-07-07 14:55:37 -0700

    hot fix

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- *mul - mlutiplies two ints.
+ *mul - multiplies two ints.
  *@a: integer.
  *@b: integer.
-*
+ *
  *Return: product.
-*/
+ */
 int mul(int a, int b)
 {
-	int product:
+	int product;
 
 	product = a * b;
 	return (product);
