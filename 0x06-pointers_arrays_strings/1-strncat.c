89ee588b78c18cd5d86bb93edb790ab91bda5183
AatifeRukky <iyanuoluwaaatife@gmail.com>
2022-07-14 13:42:22 -0700

    try 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strncat 
+* @dest:input string
+* @src: input string
+* @n: times
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int a;
+int b;
+a = 0
+while (dest[a] != 0)
+{
+	a++;
+}
+b = 0;
+while (src[b] != 0 && b < n)
+{
+	dest[a] = src[b];
+	a++;
+	b++;
+}
+return (dest);
+}
