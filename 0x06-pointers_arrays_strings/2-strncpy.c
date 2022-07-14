b9fcdf15beccda09614fb4367b68dfafb3c4aa7c
AatifeRukky <iyanuoluwaaatife@gmail.com>
2022-07-14 14:18:12 -0700

    try 1

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+8 _strncpy - copie the string
+* @dest: array taht returns
+* @src: pointer recives the string
+* @n: times
+* Return: the parameter dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+while (i < n && src[i] != '\0')
+{
+	dest[i] = src[i];
+	i++;
+}
+i = i;
+while (i < n)
+{
+	dest[i] = '\0';
+	i++;
+}
+return (dest);
+}
