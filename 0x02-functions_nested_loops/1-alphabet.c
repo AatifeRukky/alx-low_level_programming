1c83c064df486568d5006c654917365cca20141e
AatifeRukky <iyanuoluwaaatife@gmail.com>
2022-07-15 02:01:35 -0700

    try 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-* print_alphabet - entry point
+* print_alphabet -entry point
 * Return: 0
 */
 void print_alphabet(void)
 {
-char = 'a';
+char c = 'a';
 while (c <= 'z')
 {
-	_putchar(c);
-	c++;
+_putchar(c);
+c++;
 }
 _putchar('\n');
 }
